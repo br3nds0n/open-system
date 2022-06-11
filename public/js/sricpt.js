55b8fb829bf762e3f8146a625095f7239acfba65
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2022-06-11 14:14:10 -0300

    feat: created user form

---
+++ public/js/sricpt.js
@@ -0,0 +1,11 @@
+let button = document.querySelector(".cadastrar")
+let formulario = document.querySelector(".form-cadastro")
+
+
+formulario.addEventListener('submit', cadastrar)
+
+function cadastrar(event){
+    event.preventDefault()
+}
+
+
